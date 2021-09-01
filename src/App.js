35ed69b316f735939a7f155f8417f464650ad178
James Pickering <james.pickering@gmail.com>
2021-09-01 15:49:33 +0100

    tailwinds setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-2 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl'>
+      <div className='md:flex'>
+        <div className='md:flex-shrink-0'>
+          <img
+            className='h-48 w-full object-cover md:h-full md:w-48'
+            src='/img/store.jpg'
+            alt='Man browsing a store'
+          />
+        </div>
+        <div className='p-8'>
+          <div className='uppercase tracking-wide text-sm text-indigo-500 font-semibold'>Case study</div>
+          <a href='./' className='block mt-1 text-lg leading-tight font-medium text-black hover:underline'>
+            Finding customers for your new business
+          </a>
+          <p className='mt-2 text-gray-500'>
+            Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your
+            first customers.
+          </p>
+        </div>
+      </div>
+      <div className='mt-4 mb-4 text-center'>
+        <p>Hello Tailwind CSS</p>
+      </div>
     </div>
   );
 }
