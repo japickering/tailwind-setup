0db61e3f809fbc4f840135574d9cd1ea1039cbc3
James Pickering <james.pickering@gmail.com>
2021-09-02 12:42:33 +0100

    update tailwind cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,17 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+    },
     screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
     },
   },
-  variants: {
-    extend: {
-      backgroundColor: ['active'],
-    }
-  },
+  variants: {},
   plugins: [],
 }
